87a5ff995132decd6a8506e49f1b550f2b94603b
muhammadshiva <shivamuhammad07@gmail.com>
2021-03-30 15:18:43 +0700

    feat(model): Set fillable value

--- app/Models/Wholesales.php
+++ app/Models/Wholesales.php
@@ -3,9 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model; // Model Eloquent
 
 class Wholesales extends Model
 {
-    use HasFactory;
+    // Eloquent akan membuat model wholesales dan menyimpan record di tabel wholesales
+    protected $table = "wholesales"; 
+    public $timestamps = false; 
+    // Memanggil isi DB dengan primary key
+    protected $primaryKey = 'id';
+    
+     /**
+     * The attributes that are mass assignable. *
+     * @var array
+     */
+
+    protected $fillable = [
+        'brand',
+        'model',
+        'category',
+        'transmission',
+        'price',
+        'qty',
+    ];
+
 }
