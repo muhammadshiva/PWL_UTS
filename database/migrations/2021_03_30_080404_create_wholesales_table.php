5b40a1695f4c3a134ee2fe13dac4a50b9412212f
muhammadshiva <shivamuhammad07@gmail.com>
2021-03-30 15:12:09 +0700

    feat(migration): Integrate table wholesales

---
+++ database/migrations/2021_03_30_080404_create_wholesales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWholesalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wholesales', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 50)->nullable();
+            $table->string('model', 20)->nullable();
+            $table->string('category', 50)->nullable();
+            $table->string('transmission', 20)->nullable();
+            $table->integer('price')->nullable();
+            $table->integer('qty')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wholesales');
+    }
+}
